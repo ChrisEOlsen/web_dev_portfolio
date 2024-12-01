6dfa7d4983db1e2a601cff658c395de32408bfc5
Chris Olsen <112782812+ChrisEOlsen@users.noreply.github.com>
2024-12-01 10:13:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Next.js Template
+
+A reusable Next.js template for full-stack web applications with Tailwind CSS and DaisyUI.
---
+++ src/components/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className='p-4 bg-secondary text-white'>Footer Component</footer>;
+}
